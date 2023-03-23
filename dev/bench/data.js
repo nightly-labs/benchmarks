d8003ba4da8197db2d1b9da4a5a20029d574ffa4
github-action-benchmark <github@users.noreply.github.com>
2023-03-23 15:05:36 +0000

    add Rust Benchmark (cargo) benchmark result for 69500103a3dac6381e760f410ea39d18a2515106

---
+++ dev/bench/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1679583936403,
+  "repoUrl": "https://github.com/nightly-labs/benchmarks",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "nightly-labs",
+            "username": "nightly-labs"
+          },
+          "committer": {
+            "name": "nightly-labs",
+            "username": "nightly-labs"
+          },
+          "id": "69500103a3dac6381e760f410ea39d18a2515106",
+          "message": "test",
+          "timestamp": "2023-03-23T14:51:11Z",
+          "url": "https://github.com/nightly-labs/benchmarks/pull/1/commits/69500103a3dac6381e760f410ea39d18a2515106"
+        },
+        "date": 1679583935910,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 20",
+            "value": 21089,
+            "range": "± 6",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
