4f60cbb0109476eaafd35977799fa8d2a03922b2
github-action-benchmark <github@users.noreply.github.com>
2023-03-24 13:05:13 +0100

    add Rust Benchmark (cargo) benchmark result for df2bdae18e62e701db5d260d9ac0752213c02ff8

---
+++ data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1679659513519,
+  "repoUrl": "https://github.com/nightly-labs/exchange",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "norbertbodziony@gmail.com",
+            "name": "NB",
+            "username": "NorbertBodziony"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "df2bdae18e62e701db5d260d9ac0752213c02ff8",
+          "message": "Merge pull request #324 from nightly-labs/e2e-bench\n\nE2e bench",
+          "timestamp": "2023-03-24T12:42:56+01:00",
+          "tree_id": "d7fa0edc1666947d542dd212349de9c7fd57f8cc",
+          "url": "https://github.com/nightly-labs/exchange/commit/df2bdae18e62e701db5d260d9ac0752213c02ff8"
+        },
+        "date": 1679659511587,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "WS/get_user_data_benchmark",
+            "value": 2089258,
+            "range": "± 56939",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
