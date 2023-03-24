61a8ad1f442d20fe16a1ea190489bba8cc0ca092
github-action-benchmark <github@users.noreply.github.com>
2023-03-24 16:15:05 +0100

    add Rust Benchmark (cargo) benchmark result for 462c62c739eb292647b0bed40ac1f07bd9063e2a

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679659513519,
+  "lastUpdate": 1679670905826,
   "repoUrl": "https://github.com/nightly-labs/exchange",
   "entries": {
     "Rust Benchmark": [
@@ -29,6 +29,36 @@ window.BENCHMARK_DATA = {
             "name": "WS/get_user_data_benchmark",
             "value": 2089258,
             "range": "± 56939",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "norbertbodziony@gmail.com",
+            "name": "NB",
+            "username": "NorbertBodziony"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "462c62c739eb292647b0bed40ac1f07bd9063e2a",
+          "message": "Merge pull request #365 from nightly-labs/refactor-get-user-data-to-channel\n\nrefactor get user data to channel",
+          "timestamp": "2023-03-24T15:57:53+01:00",
+          "tree_id": "ea0490889453ca7196f50eb5643042cef56628da",
+          "url": "https://github.com/nightly-labs/exchange/commit/462c62c739eb292647b0bed40ac1f07bd9063e2a"
+        },
+        "date": 1679670905026,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "WS/get_user_data_benchmark",
+            "value": 65829,
+            "range": "± 12959",
             "unit": "ns/iter"
           }
         ]
